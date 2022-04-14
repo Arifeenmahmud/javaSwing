906e077658ca0736545aba57c4f2814784425fe5
Arifeen Mahmud <arifeen.mahmud@yahoo.com>
2022-04-15 01:49:43 +0600

    Jframe Form Menubar

---
+++ src/jframeformadddelete/JframeFormMenubar.java
@@ -0,0 +1,21 @@
+package jframeformadddelete;
+
+/**
+ *
+ * @author Arifeen
+ */
+class JframeFormAddDelete {
+
+    /**
+     * @param args the command line arguments
+     */
+
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        jframeformadddelete.Menubar ms= new jframeformadddelete.Menubar();
+        ms.setBounds(250, 150, 300, 400);
+        ms.setVisible(true);
+    }
+
+}
