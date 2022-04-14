9549d791454fe3d65745c378b9c5afea83bf1a00
Arifeen Mahmud <arifeen.mahmud@yahoo.com>
2022-04-15 00:48:34 +0600

    productIdBtnStkValidate

---
+++ src/invoice/Invoice.java
@@ -0,0 +1,20 @@
+package invoice;
+
+/**
+ *
+ * @author Arifeen
+ */
+public class Invoice {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        ProductInfo info = new ProductInfo();
+
+        info.setBounds(400, 50, 500, 300);
+        info.setVisible(true);
+
+    }
+
+}
