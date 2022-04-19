90ba7731f9c8d9b02b9bcb47431fa6e99835a39e
Arifeen Mahmud <arifeen.mahmud@yahoo.com>
2022-04-19 16:06:08 +0600

    Evedence 20.04.2022

---
+++ src/evidence/Evidence.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package evidence;
+
+/**
+ *
+ * @author Arifeen Mahmud
+ */
+public class Evidence {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        info fo = new info();
+        fo.setBounds(450, 100, 400, 400);
+        fo.setVisible(true);
+    }
+
+}
