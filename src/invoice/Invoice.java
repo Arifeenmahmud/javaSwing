254b417e15d4f62683bacf4934800f4dce66a66c
Arifeen Mahmud <arifeen.mahmud@yahoo.com>
2022-04-20 16:26:04 +0600

    Oracle Database Connection

---
+++ src/invoice/Invoice.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package invoice;
+
+/**
+ *
+ * @author Arifeen
+ */
+public class Invoice {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        invoice.ProductInfo info = new invoice.ProductInfo();
+
+        info.setBounds(400, 50, 500, 300);
+        info.setVisible(true);
+
+    }
+
+}
